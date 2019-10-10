829526e05c5fdac5a10253fa25830ad7c1d91cf5
kavya kh <kavyakh12@gmail.com>
2019-10-09 19:29:28 -0700

    added simpleBeanXmlanno

---
+++ SimpleBeanXmlAnno/src/main/java/com/zensar/spring/main/SpringMain.java
@@ -0,0 +1,22 @@
+package com.zensar.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.zensar.spring.Greeting;
+
+//creating the Obj of spring IOC container
+
+public class SpringMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("greetings.xml");
+		Greeting g = ctx.getBean("festival", Greeting.class); 
+		//System.out.println(g.greet());
+		
+		Greeting g1 = ctx.getBean("anniversary", Greeting.class); 
+		//System.out.println(g1.greet());
+	
+	}
+ 
+}
